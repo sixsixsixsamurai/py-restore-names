39af1a0f6cee71a57089cd9767adfa9e60f61cc5
Mykola Medved <kradu2001@gmail.com>
2024-04-15 03:37:26 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,38 @@
-import pytest
+from unittest import TestCase
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+class TestName(TestCase):
+
+    def setUp(self) -> None:
+        self.users = [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
+
+    def tearDown(self) -> None:
+        del self.users
+
+    def test_restore_expected(self) -> None:
+        restore_names(self.users)
+
+        assert self.users == [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
